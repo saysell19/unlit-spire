94de785e041fbf8e55d12a292defb041c3299be9
Billy <53446945+saysell19@users.noreply.github.com>
2023-05-10 22:06:12 +0100

    Started building Main
    Construction of the main game loop has begun. placeholder background image containing the very, very rough layout has been added too!

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 # main module for the game
-# All objects will be creted here
-# Game loop will be here too
+
+import pygame
+
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("The Unlit Spire")
+
+background = pygame.image.load('assets/bg1-unlit-spire.jpg').convert_alpha()
+# function to draw the background
+def draw_bg():
+    screen.blit(background, (0, 0))
+
+
+# Game loop
+run = True
+while run:
+
+    # draw background
+    draw_bg()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # update display window
+    pygame.display.update()
+
+pygame.quit()
